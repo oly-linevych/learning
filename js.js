5f8ac68b36484c9f5d9b17269794e83bec63395a
oly-linevych <115529742+oly-linevych@users.noreply.github.com>
2023-08-07 22:45:19 +0300

    ГУД
    ГУД

---
+++ js.js
@@ -0,0 +1,25 @@
+// ------------------1. ПРИ КЛІКУ ЗМІНЮЄТЬСЯ КОЛІР КНОПКИ----------------------------
+// const tagsContainer = document.querySelector('.js-tags');
+// console.log(tagsContainer);
+
+
+// tagsContainer.addEventListener('click', onTagsContainerClick)
+
+// function onTagsContainerClick (evt){
+//     if (evt.target.nodeName !== 'BUTTON') {
+//         return
+//     }
+
+//     console.dir(evt.target)
+//     console.dir(evt.currentTarget)
+
+// const currentActive = document.querySelector('.tags__btn-active')
+//     console.log(currentActive)
+
+//     if (currentActive) {
+// currentActive.classList.remove('tags__btn-active')
+//     }
+//     evt.target.classList.add('tags__btn-active')
+//     }
+
+
